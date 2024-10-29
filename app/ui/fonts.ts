8b6389417d35ac230a3d01167095314d9fdacebe
pytechlady <username@Usernames-MacBook-Air.local>
2024-10-29 12:11:30 +0000

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subset: ['latin'] });
+
+export const lusitana = Lusitana({ subset: ['latin'], weight: ['400', '700'] });
